756b2db0a9e1982e2a18f979ec9ced3e6f0fd3ed
Ayush Kumar <ayush.k1501@gmail.com>
2023-08-25 18:08:42 +0530

    profile card completed

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Styles from './ProfileCard.module.css';
+import Logo from '../../logo.svg';
 const ProfileCard = () => {
     return (
         <div className={Styles.card}>
             <div className={Styles.banner}>
+            </div>
+            <div className={Styles.profile}>
+                <div className={Styles.profileImage}>
+                    <img src={Logo} alt="profile" />
+                </div>
+                <div className={Styles.profileInfo}>
+                    <h3>John Doe</h3>
+                    <h4>@username</h4>
+                </div>
+            </div>
+            <div className={Styles.profileStats}>
+                <div className={Styles.followingCount}>
+                    <h3>100</h3>
+                    <h4>Following</h4>
+                </div>
+                <div className={Styles.followersCount}>
+                    <h3>100</h3>
+                    <h4>Followers</h4>
+                </div>
+            </div>
+            <div className={Styles.FindPeople}>
+                <h3>Find new People</h3>
             </div>
         </div>
     )
