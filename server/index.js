696ad708c2431daeb163de32a7549567e1ef7851
Ayush Kumar <ayush.k1501@gmail.com>
2023-09-22 03:50:00 +0530

    patch 32

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get('/api/checkToken', auth, (req, res) => {
 
 app.get('/*', (req, res)=> {
     // res.sendFile(path.join(__dirname, process.env.PUBLIC_DIR, 'index.html'));
-    res.sendFile(path.join(path.resolve(__dirname, '/build/index.html')));
+    res.sendFile(path.resolve(__dirname, '/build/index.html'));
 });
 
 app.listen(port, () => {
